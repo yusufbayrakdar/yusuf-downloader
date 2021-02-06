1b16fba962ffdceb985b372250a5c3a3375b2245
bayrakdar <bayrakdar@bayrakdar-MacBook-Air.local>
2021-02-06 21:19:33 +0300

    downloader server init

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use((err, req, res, next) => {
+	console.error(err);
+	if (err.status) {
+		res.sendStatus(err.status);
+	} else {
+		res.sendStatus(500);
+	}
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+const server = {
+	name: 'downloader',
+	port: process.env.PORT || 3811
+};
+// Start server
+app.listen(server.port, () => {
+	console.info(`${server.name} listening at ${server.port}`);
+});
