1655290f27d9deb552e35beef6c5d8a3389a494a
Sabri <sabri.bayrakdar@boun.edu.tr>
2021-02-21 03:41:20 +0300

    file path fixing

--- chrome-extention/background.js
+++ chrome-extention/background.js
@@ -1,21 +1,26 @@
 async function downloadVideo() {
-    chrome.tabs.getSelected(null, tab => {
-        let url = tab.url;
-        $("#loader").css("display", "block");
-        $("#video-image").css("filter", "brightness(50%)");
-        chrome.downloads.download({
-            method: 'GET',
-            url: `https://yusuf-downloader.herokuapp.com/download/mp3/?url=${url}`
-        }, () => {
-            $("#loader").css("display", "none");
-            $("#video-image").css("filter", "brightness(100%)");
-        })
-    });
+  chrome.tabs.getSelected(null, tab => {
+    let url = tab.url;
+    $("#loader").css("display", "block");
+    $("#video-image").css("filter", "brightness(50%)");
+    chrome.downloads.download(
+      {
+        method: "GET",
+        url: `https://yusuf-downloader.herokuapp.com/download/mp3/?url=${url}`
+      },
+      () => {
+        $("#loader").css("display", "none");
+        $("#video-image").css("filter", "brightness(100%)");
+      }
+    );
+  });
 }
 function getUrl() {
-    chrome.tabs.getSelected(null, tab => {
-        let url = tab.url;
-        let videoId = url.replace(/(.+watch\?v=|&.+)/g, '');
-        document.getElementById("video-image").src = `https://img.youtube.com/vi/${videoId}/0.jpg`
-    });
+  chrome.tabs.getSelected(null, tab => {
+    let url = tab.url;
+    let videoId = url.replace(/(.+watch\?v=|&.+)/g, "");
+    document.getElementById(
+      "video-image"
+    ).src = `https://img.youtube.com/vi/${videoId}/0.jpg`;
+  });
 }
